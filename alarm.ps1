e139f833f7e18a3b7a4f9425fd5800282d8cb7a5
lionel-git <lionel.desorme@free.fr>
2019-06-01 20:15:08 +0200

    Test notif

--- alarm.ps1
+++ alarm.ps1
@@ -2,13 +2,32 @@
 param (
     [double]$mins = 1
  )
+ 
+Add-Type -AssemblyName System.Windows.Forms 
+
+function TestNotif { 
+$global:balloon = New-Object System.Windows.Forms.NotifyIcon
+$path = (Get-Process -id $pid).Path
+$balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
+$balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning 
+$balloon.BalloonTipText = '*Alarm is finished*'
+$balloon.BalloonTipTitle = "Attention $Env:USERNAME" 
+$balloon.Visible = $true 
+$balloon.ShowBalloonTip(5000)
+}
+ 
 $delay = $mins*60
 echo "Waiting for $delay seconds ..."
 Start-Sleep -Seconds $delay
 echo "Finished !!!"
-For ($i=0; $i -le 10; $i++) 
+TestNotif
+
+For ($i=0; $i -le 3; $i++) 
 {
 	[console]::beep(600,200)
     Start-Sleep -Seconds 2
 }
+
+
+
 
